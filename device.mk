5fa520db78c0436db5bc7ba12ee7d5c858b3aebe
mranonymz <mprabhat774@gmail.com>
2020-11-10 20:43:29 +0530

    X00TD:liber: prebuilts: Add Retro Music Player as prebuilt
    Signed-off-by: mranonymz <mprabhat774@gmail.com>

--- device.mk
+++ device.mk
@@ -82,6 +82,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal
 
+# Retro
+PRODUCT_PACKAGES += \
+    RMPlayer
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
